e5c4c627d50bf819317e0b758655722694eda370
Taras Leskiv <leskiv.taras@gmail.com>
2023-08-30 22:48:41 +0300

    small tweaks to debug UI

--- js/src/lib/scene/FullScreenScene.ts
+++ js/src/lib/scene/FullScreenScene.ts
@@ -65,19 +65,21 @@ export class FullScreenScene extends THREE.Scene {
         this.add(this.axesHelper)
 
         const gui = new GUI();
-        let folder = gui.addFolder("Axes");
 
-        folder.add(this.debugControls, 'showAxesHelper').name("Show Axes")
+        let folder = gui.addFolder("Axes");
+        folder.add(this.debugControls, 'showAxesHelper').name("Is Visible")
             .onChange(() => {
                 this.axesHelper.visible = !this.axesHelper.visible
             })
-        folder.add(this.debugControls, 'axesSize')
+        folder.add(this.debugControls, 'axesSize').name("Size")
             .min(1).max(10).step(0.5)
             .onFinishChange(() => {
-                this.axesHelper.removeFromParent()
+                this.remove(this.axesHelper)
                 this.axesHelper.dispose()
                 this.axesHelper = new THREE.AxesHelper(this.debugControls.axesSize)
                 this.add(this.axesHelper)
             })
+
+        gui.addFolder("Orbit controls")
     }
 }
