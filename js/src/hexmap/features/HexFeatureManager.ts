b052f9b9c383fb543142e85ca769b42419fbab62
Taras Leskiv <leskiv.taras@gmail.com>
2023-10-22 19:08:52 +0300

    [Part 9] Dispose feature object

--- js/src/hexmap/features/HexFeatureManager.ts
+++ js/src/hexmap/features/HexFeatureManager.ts
@@ -1,16 +1,24 @@
-import {Scene, Vector3} from "three";
+import {Group, Scene, Vector3} from "three";
 import {CubeFeature} from "./CubeFeature";
 import {HexMetrics} from "../HexMetrics";
 import {Vec3} from "../../lib/math/Vec3";
 
 export class HexFeatureManager {
     private _scene: Scene;
+    private _container: Group;
 
     constructor(scene: Scene) {
         this._scene = scene;
+        this._container = new Group();
+        this._container.name = "Features container";
+        this._scene.add(this._container);
     }
 
     clear(): void {
+        if (this._container) {
+            console.log(this._container.children.length);
+            this._container.clear();
+        }
     }
 
     apply(): void {
@@ -25,5 +33,6 @@ export class HexFeatureManager {
 
         const worldPos = HexMetrics.perturb(position);
         instance.position.copy(Vec3.add(worldPos, instance.position));
+        this._container.attach(instance);
     }
 }
