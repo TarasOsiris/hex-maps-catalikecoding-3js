9f474204404ea849d4c0e33261e3e3f4ffc5e3a1
Taras Leskiv <leskiv.taras@gmail.com>
2023-11-11 22:31:12 +0200

    [Part 10] Walls 2.5 / better wireframe on walls

--- js/src/hexmap/util/HexMaterials.ts
+++ js/src/hexmap/util/HexMaterials.ts
@@ -8,85 +8,89 @@ import {WaterShoreMaterial} from "../rendering/WaterShoreMaterial";
 import {EstuariesMaterial} from "../rendering/EstuariesMaterial";
 
 export class HexMaterials {
-    private static waterColor = new Color(0x4069ff);
+	private static waterColor = new Color(0x4069ff);
 
-    static readonly terrainMaterial = new MeshStandardMaterial({
-        vertexColors: true,
-        polygonOffset: true,
-        polygonOffsetFactor: 2,
-        polygonOffsetUnits: 2
-    });
+	static readonly terrainMaterial = new MeshStandardMaterial({
+		vertexColors: true,
+		polygonOffset: true,
+		polygonOffsetFactor: 2,
+		polygonOffsetUnits: 2
+	});
 
-    // TODO inline when possible
-    private static riverUniforms: RiverUniforms;
-    private static waterUniforms: WaterUniforms;
-    private static roadUniforms: RoadUniforms;
+	// TODO inline when possible
+	private static riverUniforms: RiverUniforms;
+	private static waterUniforms: WaterUniforms;
+	private static roadUniforms: RoadUniforms;
 
-    static riverMaterial: RiverMaterial;
-    static waterMaterial: WaterMaterial;
-    static waterShoreMaterial: WaterShoreMaterial;
-    static estuariesMaterial: WaterShoreMaterial;
-    static roadMaterial: RoadMaterial;
+	static riverMaterial: RiverMaterial;
+	static waterMaterial: WaterMaterial;
+	static waterShoreMaterial: WaterShoreMaterial;
+	static estuariesMaterial: WaterShoreMaterial;
+	static roadMaterial: RoadMaterial;
 
-    static createMaterials(noiseTexture: Texture) {
-        this.createRiverMaterial(noiseTexture);
-        this.createWaterMaterial(noiseTexture);
-        this.createRoadMaterial(noiseTexture);
-    }
+	static createMaterials(noiseTexture: Texture) {
+		this.createRiverMaterial(noiseTexture);
+		this.createWaterMaterial(noiseTexture);
+		this.createRoadMaterial(noiseTexture);
+	}
 
-    static createRiverMaterial(noiseTexture: Texture) {
-        this.riverUniforms = {
-            time: {value: 0},
-            noiseTexture: {value: noiseTexture},
-            waterColor: {value: this.waterColor}
-        };
-        this.riverMaterial = new RiverMaterial(this.riverUniforms);
-    }
+	static createRiverMaterial(noiseTexture: Texture) {
+		this.riverUniforms = {
+			time: {value: 0},
+			noiseTexture: {value: noiseTexture},
+			waterColor: {value: this.waterColor}
+		};
+		this.riverMaterial = new RiverMaterial(this.riverUniforms);
+	}
 
-    static createWaterMaterial(noiseTexture: Texture) {
-        this.waterUniforms = {
-            time: {value: 0},
-            waterColor: {value: this.waterColor},
-            noiseTexture: {value: noiseTexture}
-        };
-        this.waterMaterial = new WaterMaterial(this.waterUniforms);
-        this.waterShoreMaterial = new WaterShoreMaterial(this.waterUniforms);
-        this.estuariesMaterial = new EstuariesMaterial(this.waterUniforms);
-    }
+	static createWaterMaterial(noiseTexture: Texture) {
+		this.waterUniforms = {
+			time: {value: 0},
+			waterColor: {value: this.waterColor},
+			noiseTexture: {value: noiseTexture}
+		};
+		this.waterMaterial = new WaterMaterial(this.waterUniforms);
+		this.waterShoreMaterial = new WaterShoreMaterial(this.waterUniforms);
+		this.estuariesMaterial = new EstuariesMaterial(this.waterUniforms);
+	}
 
-    static createRoadMaterial(noiseTexture: Texture) {
-        this.roadUniforms = {
-            roadColor: {value: new Color(0xff0000)},
-            noiseTexture: {value: noiseTexture}
-        };
-        this.roadMaterial = new RoadMaterial(this.roadUniforms);
-    }
+	static createRoadMaterial(noiseTexture: Texture) {
+		this.roadUniforms = {
+			roadColor: {value: new Color(0xff0000)},
+			noiseTexture: {value: noiseTexture}
+		};
+		this.roadMaterial = new RoadMaterial(this.roadUniforms);
+	}
 
-    static updateTime(elapsedTime: number) {
-        this.riverMaterial?.updateTime(elapsedTime);
-        this.waterMaterial?.updateTime(elapsedTime);
-        this.waterShoreMaterial?.updateTime(elapsedTime);
-        this.estuariesMaterial?.updateTime(elapsedTime);
-    }
+	static updateTime(elapsedTime: number) {
+		this.riverMaterial?.updateTime(elapsedTime);
+		this.waterMaterial?.updateTime(elapsedTime);
+		this.waterShoreMaterial?.updateTime(elapsedTime);
+		this.estuariesMaterial?.updateTime(elapsedTime);
+	}
 
-    static readonly wireframeMaterial = new MeshBasicMaterial({
-        wireframe: true,
-        color: 0x000000,
-    });
-    static readonly debugMaterial = new MeshBasicMaterial({
-        color: 0xff0000,
-        polygonOffset: true,
-        polygonOffsetFactor: 1,
-        polygonOffsetUnits: 1
-    });
-    static readonly fontMaterial = new MeshBasicMaterial({color: 0xff0000});
+	static readonly wireframeMaterial = new MeshBasicMaterial({
+		wireframe: true,
+		color: 0x000000,
+	});
+	static readonly debugMaterial = new MeshBasicMaterial({
+		color: 0xff0000,
+		polygonOffset: true,
+		polygonOffsetFactor: 1,
+		polygonOffsetUnits: 1
+	});
+	static readonly fontMaterial = new MeshBasicMaterial({color: 0xff0000});
 
-    static testMat = new ShaderMaterial({
-        vertexShader: tVertex, fragmentShader: tFragment,
-        defines: {'STANDARD': ''},
-    });
+	static testMat = new ShaderMaterial({
+		vertexShader: tVertex, fragmentShader: tFragment,
+		defines: {'STANDARD': ''},
+	});
 
-    static readonly urbanFeatureMaterial = new MeshStandardMaterial({color: 'red'});
-    static readonly farmFeatureMaterial = new MeshStandardMaterial({color: 0xacdd22});
-    static readonly plantFeatureMaterial = new MeshStandardMaterial({color: 0x319223});
+	static readonly urbanFeatureMaterial = new MeshStandardMaterial({
+		color: 'red', polygonOffset: true,
+		polygonOffsetFactor: 2,
+		polygonOffsetUnits: 2
+	});
+	static readonly farmFeatureMaterial = new MeshStandardMaterial({color: 0xacdd22});
+	static readonly plantFeatureMaterial = new MeshStandardMaterial({color: 0x319223});
 }
